24b770e6be11e40f05c5e138c735ca3163b9f9ca
yuraoliynyk <yuraoliynyk.20@gmail.com>
2021-08-19 14:26:38 +0300

    created lesson5-classwork

---
+++ src/services/car.service.js
@@ -0,0 +1,14 @@
+import {axios} from 'axios';
+
+const config = {
+    baseURL: 'http://192.168.1.253/api/v1/cars'
+}
+
+// IP с дома 195.72.146.25
+// IP в офисе 192.168.1.253
+
+function getCars() {
+    axios.create(config).get();
+}
+
+export {getCars};
